da65e90276c618d4cfc2619048751c47bcf029fc
Brian Korir <144766290+Brian-Korir@users.noreply.github.com>
2024-05-18 21:27:45 +0300

    Update bet.py

--- bet.py
+++ bet.py
@@ -21,8 +21,7 @@ from datetime import datetime
 def calculate_age(birthdate):
 today= date.time(today)
 age = today.year - birthdate.year -((today.month, today.day <
- (birthdate.month,
-
+ (birthdate.month,birthdate.day
 
 def check_winnings(columns, lines, bet, values):
    winnings = 0
